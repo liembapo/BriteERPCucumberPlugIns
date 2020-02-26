d9d853778e0ccc971e6c5e4329b075b8e04e7288
AntonSemenov <Anthony04061987@gmail.com>
2020-02-26 13:45:33 -0600

    added test case about Organization Chart test case 205

---
+++ src/test/java/step_definitions/briteERPEmployeeModule/Ability_to_see_OC.java
@@ -0,0 +1,17 @@
+package step_definitions.briteERPEmployeeModule;
+
+import cucumber.api.java.en.Then;
+import org.junit.Assert;
+import pages.EmployeeDetailPage;
+import pages.EmployeeTabPage;
+
+public class Ability_to_see_OC {
+
+    EmployeeTabPage employeeTabPage =new EmployeeTabPage();
+    EmployeeDetailPage employeeDetailPage = new EmployeeDetailPage();
+
+    @Then("User should be able to see Organization Chart")
+    public void user_should_be_able_to_see_Organization_Chart() {
+        Assert.assertTrue("Organization Chart is NOT displayed",employeeDetailPage.organizationChart.isDisplayed());
+    }
+}
