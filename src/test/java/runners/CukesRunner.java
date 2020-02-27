35a9888c001b68c0a530116f649cdb2f7c3e85a3
Valentyn <valentyn.atroshchenko@gmail.com>
2020-02-27 15:10:06 -0600

    added plugins to CukesRunner

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -7,11 +7,13 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-reports",
+        plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json",
+                "junit:target/cucumber-reports/Cucumber.xml",
+                "html:target/cucumber-reports"},
         features = "src/test/resources/features",
         glue = "step_definitions",
-        dryRun = true,
-        tags = "@logTest"
+        dryRun = false,
+        tags = "@ValentynTests"
 
 //        tags = "@checkSelectOption"
 
